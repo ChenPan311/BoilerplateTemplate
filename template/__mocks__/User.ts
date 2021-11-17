c33e4da62618b883f55d79d4e8de775b186716ae
ChenPanker <chenpanker18@gmail.com>
2021-11-17 12:50:44 +0200

    bug fix

--- template/__mocks__/User.ts
+++ template/__mocks__/User.ts
@@ -1,4 +1,4 @@
 import { axiosMockAdapterInstance } from '../App/Utils/axios'
 
-axiosMockAdapterInstance.onAny('')
+axiosMockAdapterInstance.onAny()
     .reply(200, { results: [{ id: 1, name: "Chen Panker", email: 'chen@test.com' }] });
